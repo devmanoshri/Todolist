266601365eba67583d5c76c64f62b9d8c5fbbdc4
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2025-12-21 10:28:13 +0100

    add search functionality

--- src/app/components/todo-items/todo-items.component.ts
+++ src/app/components/todo-items/todo-items.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-items',
-  imports: [HighlightCompletedTodoDirective],
+  imports: [HighlightCompletedTodoDirective, UpperCasePipe],
   templateUrl: './todo-items.component.html',
   styleUrl: './todo-items.component.scss'
 })
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,16 +3,19 @@ import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { TodoItemsComponent } from "../components/todo-items/todo-items.component";
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemsComponent],
+  imports: [TodoItemsComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit {
   todosService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     this.todosService.getTodosFromApi().pipe(
