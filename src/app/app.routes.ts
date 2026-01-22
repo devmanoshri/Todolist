4d0955a8fe663a06f6d83136f1ad9814c2f06348
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-22 16:55:59 +0100

    add cart component and bootstrP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,24 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'prctice',
-        loadComponent: () => {
-            return import('./home/home.component').then(m => m.HomeComponent)
-        }
+  {
+    path: 'prctice',
+    loadComponent: () => {
+      return import('./home/home.component').then((m) => m.HomeComponent);
     },
-    {
-        path: '',
-        loadComponent: () => {
-            return import('./todos/todos.component').then(m => m.TodosComponent)
-        }
-    }
+  },
+  {
+    path: 'cart',
+    loadComponent: () => {
+      return import('./components/cart/cart.component').then(
+        (m) => m.CartComponent
+      );
+    },
+  },
+  {
+    path: '',
+    loadComponent: () => {
+      return import('./todos/todos.component').then((m) => m.TodosComponent);
+    },
+  },
 ];
